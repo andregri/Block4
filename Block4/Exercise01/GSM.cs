68b40185a342317f6879acdb26d71604087e1cd4
michisalvo <jimmyginooo@libero.it>
2016-11-20 19:33:35 +0100

    Add GSM class

---
+++ Block4/Exercise01/GSM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    public class GSM
+    {
+        private string model = null;
+        private double price = 0;
+        private string manufacturer = null;
+        private string owner = null;
+        private Battery battery;
+        private Display display;
+        static GSM samsungGalaxyS7;
+    }
+}
