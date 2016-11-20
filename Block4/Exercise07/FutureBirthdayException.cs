7be7799ba835a574a81d4b6729c6956ab6f5c71e
andregri <andrea.grillo96@live.com>
2016-11-20 19:51:10 +0100

    add custom exceptions

---
+++ Block4/Exercise07/FutureBirthdayException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise07
+{
+    public class FutureBirthdayException : ApplicationException
+    {
+        public DateTime BirthDate
+        {
+            get;
+            set;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "Birth date (" + BirthDate.ToString() + ") couldn't be after current date(" + DateTime.Now.ToString() + ")";
+            }
+        }
+
+        public FutureBirthdayException(DateTime date, Exception inner)
+            : base(null, inner)
+        {
+            BirthDate = date;
+        }
+
+        public FutureBirthdayException(DateTime date)
+            : this(date, null) { }
+    }
+}
---
+++ Block4/Exercise07/InvalidMailAddressException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise07
+{
+    public class InvalidMailAddressException : Exception
+    {
+        private string email;
+
+        public override string Message
+        {
+            get
+            {
+                return email + " is not a valid email address.";
+            }
+        }
+
+        public InvalidMailAddressException(string email, Exception inner)
+            : base(null, inner)
+        {
+            this.email = email;
+        }
+
+        public InvalidMailAddressException(string email)
+            : this(email, null) {
+        }
+    }
+}
