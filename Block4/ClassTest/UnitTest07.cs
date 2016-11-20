ea4d10cbda34fb6fbcbed5b8449762efafb81f5f
andregri <andrea.grillo96@live.com>
2016-11-20 19:51:40 +0100

    add member and constructors with test

--- Block4/ClassTest/UnitTest07.cs
+++ Block4/ClassTest/UnitTest07.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise07;
 
 namespace ClassTest
 {
     [TestClass]
     public class UnitTest07
     {
+        // invalid mail
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(InvalidMailAddressException))]
+        public void InvalidMailTest1()
         {
+            Person p = new Person("a", "b", "test@test");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidMailAddressException))]
+        public void InvalidMailTest2()
+        {
+            Person p = new Person("a", "b", "@gmail.com");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidMailAddressException))]
+        public void InvalidMailTest3()
+        {
+            Person p = new Person("a", "b", "a@a.b");
+        }
+
+        //invalid birth date
+        [TestMethod]
+        [ExpectedException(typeof(FutureBirthdayException))]
+        public void InvalidBirthdayTestFuture()
+        {
+            Person p = new Person("a", "b", DateTime.Now.AddDays(1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TooOldBirthdayException))]
+        public void InvalidBirthdayTestTooOld()
+        {
+            Person p = new Person("a", "b", new DateTime(1899, 1, 1));
         }
     }
 }
---
+++ Block4/Exercise07/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise07
+{
+    public class Person
+    {
+        public string FirstName
+        {
+            get; set;
+        }
+
+        public string LastName
+        {
+            get; set;
+        }
+
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                string pattern = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w{2,}([-.]\w{2,})*";
+
+                if (Regex.Matches(value, pattern).Count != 1)
+                {
+                    throw new InvalidMailAddressException(value);
+                }
+                else
+                {
+                    email = value;
+                }
+            }
+        }
+
+        private DateTime birthday;
+        public DateTime Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+            set
+            {
+                if (value.Year.CompareTo(1900) < 0)
+                {
+                    throw new TooOldBirthdayException(value);
+                }
+                else if (value.CompareTo(DateTime.Now) > 0)
+                {
+                    throw new FutureBirthdayException(value);
+                }
+                else
+                {
+                    birthday = value;
+                }
+            }
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person(string firstName, string lastName, string email)
+            : this(firstName, lastName)
+        {
+            Email = email;
+        }
+
+        public Person(string firstName, string lastName, DateTime birthday)
+            : this(firstName, lastName)
+        {
+            Birthday = birthday;
+        }
+
+        public Person(string firstName, string lastName, DateTime birthday, string email)
+            : this(firstName, lastName, email)
+        {
+            Birthday = birthday;
+        }
+    }
+}
