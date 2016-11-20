f07814d2acfa3380e6ab5c7cddf982f6c4cac196
andregri <andrea.grillo96@live.com>
2016-11-20 12:02:08 +0100

    add test for ToArray

--- Block4/ClassTest/UnitTest04.cs
+++ Block4/ClassTest/UnitTest04.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise04;
 
 namespace ClassTest
 {
     [TestClass]
     public class UnitTest04
     {
+        static DoubleLinkedList<string> sList;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            sList = new DoubleLinkedList<string>("hello", "world");
+        }
+
+        // to array
+        [TestMethod]
+        public void ToArray()
+        {
+            string[] array = sList.ToArray();
+            string[] expected = { "hello", "world" };
+
+            CollectionAssert.AreEqual(expected, array);
+        }
         [TestMethod]
         public void TestMethod1()
         {
