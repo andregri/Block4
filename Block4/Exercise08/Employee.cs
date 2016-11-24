e429223073d26a37426863e6e703e32cdb408b10
andregri <andrea.grillo96@live.com>
2016-11-24 19:32:20 +0100

    create Employee class

---
+++ Block4/Exercise08/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Exercise07;
+
+namespace Exercise08
+{
+    class Employee : Person, IPayable
+    {
+        private decimal salary;
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Salary should not be negative.");
+                }
+                salary = value;
+            }
+        }
+
+        public string MailAddress
+        {
+            get;
+            private set;
+        }
+
+        public string PaymentAddress
+        {
+            get
+            {
+                return MailAddress;
+            }
+        }
+
+        public decimal Amount
+        {
+            get
+            {
+                return Salary;
+            }
+        }
+
+        public Employee(string firstName, string lastName, DateTime birthday, string email, string mailAddress, decimal salary)
+            : base(firstName, lastName, birthday, email)
+        {
+            MailAddress = mailAddress;
+            Salary = salary;
+        }
+
+        public void AddAmount(decimal amount)
+        {
+            Salary += amount;
+        }
+    }
+}
