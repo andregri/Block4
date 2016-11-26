481526319932c32795c2c5adb42dd82f612527cc
andregri <andrea.grillo96@live.com>
2016-11-26 17:46:12 +0100

    add answers to struct questions

--- Block4/Exercise06/Time.cs
+++ Block4/Exercise06/Time.cs
@@ -62,3 +62,23 @@ namespace Exercise06
         }
     }
 }
+
+//Q: Try to declare a non-static field of type Time in the struct type Time. Why is this illegal?
+//A: Non si può creare una Struct ricorsiva perchè le struct sono "Primitive data types",
+//   pertanto sono salvate nella stack e la loro dimensione deve essere nota a priori. 
+//   Infatti se dicchiarassimo all'interno della struct un membro dello stesso tipo, non 
+//   potremmo mai calcolarne la dimensione nel momento in cui istanziamo la struct: alla 
+//   somma della dimensione di tutti i membri dovremmo aggiungere la dimensione della 
+//   struct (che però è la nostra incognita).
+//
+//Q: Why is it legal for a class to have a non-static field of the same type as the class?
+//A: Perchè le classi sono "Reference type", cioè sono un indirizzo che punta alla locazione
+//   in cui è salvato il loro contenuto. Se dichiariamo all'interno della classe, un membro 
+//   dello stesso tipo della classe, possiamo calcolare senza problemi la dimensione della
+//   classe: essa sarà la somma delle dimensioni di tutti i membri + la dimensione di un 
+//   indirizzo.
+//
+//Q: Can you declare a static field noon of type Time in the struct type? Why?
+//A: Sì, perchè i membri statici vengono istanziati una sola volta e sono comuni a tutte le
+//   istanze della struct. Inoltre possono essere usati ancora prima di istanziare una struct.
+//   
