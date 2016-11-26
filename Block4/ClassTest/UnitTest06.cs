cc9fbf9efd31da47c3be492c5e3329899bb219e2
andregri <andrea.grillo96@live.com>
2016-11-26 16:08:24 +0100

    create Time struct with test
    readonly properties, overloaded operator + and -, override ToString()

--- Block4/ClassTest/UnitTest06.cs
+++ Block4/ClassTest/UnitTest06.cs
@@ -1,14 +1,63 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise06;
 
 namespace ClassTest
 {
     [TestClass]
     public class UnitTest06
     {
-        [TestMethod]
-        public void TestMethod1()
+        int h = 23;
+        int m = 45;
+        Time t;
+
+        [TestInitialize]
+        public void InitializeClass()
         {
+            t = new Time(h, m);
+        }
+
+        // test properties
+        [TestMethod]
+        public void HourTest()
+        {
+            Assert.AreEqual(h, t.Hour);
+        }
+
+        [TestMethod]
+        public void MinuteTest()
+        {
+            Assert.AreEqual(m, t.Minute);
+        }
+
+        // test to string
+        [TestMethod]
+        public void ToStringTest1()
+        {
+            Assert.AreEqual("10:05", new Time(10, 5).ToString());
+        }
+
+        [TestMethod]
+        public void ToStringTest2()
+        {
+            Assert.AreEqual("23:45", t.ToString());
+        }
+
+        // test overload binary operators + and -
+        [TestMethod]
+        public void SumTest()
+        {
+            Time t1 = new Time(9, 30);
+            Time sum = t1 + new Time(1, 15);
+            Assert.AreEqual("10:45", sum.ToString());
+        }
+
+        [TestMethod]
+        public void DiffTest()
+        {
+            Time t1 = new Time(9, 30);
+            Time diff = t1 - new Time(1, 15);
+            Assert.AreEqual("08:15", diff.ToString());
         }
     }
 }
---
+++ Block4/Exercise06/Time.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercise06
+{
+    public struct Time
+    {
+        private readonly int minutes;
+
+        public Time(int hh, int mm)
+        {
+            this.minutes = 60 * hh + mm;
+        }
+
+        public int Hour
+        {
+            get
+            {
+                return minutes / 60;
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                return minutes % 60;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0:D2}:{1:D2}", Hour, Minute);
+        }
+
+        public static Time operator +(Time time1, Time time2)
+        {
+            int h = time1.Hour + time2.Hour;
+            int m = time1.Minute + time2.Minute;
+            return new Time(h, m);
+        }
+
+        public static Time operator -(Time time1, Time time2)
+        {
+            int h = time1.Hour - time2.Hour;
+            int m = time1.Minute - time2.Minute;
+            return new Time(h, m);
+        }
+    }
+}
