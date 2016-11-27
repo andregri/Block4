7a965d1043415833e9b8e909f00465230e9c324a
michisalvo <jimmyginooo@libero.it>
2016-11-27 01:37:02 +0100

    Add Display class

---
+++ Block4/Exercise01/Display.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media; // add PresentationCore.dll
+
+namespace Exercise01
+{
+    class Display
+    {
+        private double size;
+        private Color colorsRGB;
+
+        public double Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public Color ColorsRGB
+        {
+            get { return colorsRGB; }
+            set { colorsRGB = value; }
+        }
+
+        public Display(double size, Color myrgb)
+        {
+            Size = size;
+            ColorsRGB = myrgb;            
+        }
+
+    }
+}
