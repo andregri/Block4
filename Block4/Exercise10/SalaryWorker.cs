bb06f3127169b643928aa39e23bb22c3f9f415f6
michisalvo <jimmyginooo@libero.it>
2016-11-28 21:52:04 +0100

    Add SalaryWorker class

---
+++ Block4/Exercise10/SalaryWorker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class SalaryWorker : Employee
+    {
+        private double annualSalary;
+
+        public double Salary
+        {
+            get { return annualSalary; }
+            set
+            {
+                if (value >= 0)
+                    annualSalary = value;
+                else
+                    throw new ArgumentOutOfRangeException("Salary can not be negative!");            
+            }
+        }
+
+        public SalaryWorker(string empName, double annualSalary) : base (empName)
+        {
+            Salary = annualSalary;
+        }
+
+        public override double calcPaidCheck()
+        {
+            return (annualSalary / 12);
+        }
+    }
+}
