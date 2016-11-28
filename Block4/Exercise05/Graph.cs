58ac922233e9e241d6f4ee30baed0cb334fab3bd
michisalvo <jimmyginooo@libero.it>
2016-11-28 17:43:40 +0100

    Start exercise; create class Graph

---
+++ Block4/Exercise05/Graph.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    public class Graph
+    {
+        private List<int>[] child;
+
+        public List<int>[] Children
+        {
+            get { return child; }
+            set { child = value; }
+        }
+
+        public Graph(List<int>[] child)
+        {
+            Children = child;
+        }
+
+        public int numberOfNodes
+        {
+            get { return child.Length; }
+        }
+
+        public void FindSubgraph()
+        {
+            List<int> currentSubgraph = new List<int>();
+            bool[] visited = new bool[numberOfNodes];
+
+            for (int i = 0; i < numberOfNodes; i++)
+            {
+                if (visited[i] != true)
+                {
+                    currentSubgraph.Add());
+                }
+            }
+        }
+    }
+}
+
+/* void TraverseDFSRecursive(node)
+{
+if (not visited[node])
+{
+visited[node] = true
+print node
+foreach child node c of node
+{
+TraverseDFSRecursive(c);
+}
+}
+}*/
