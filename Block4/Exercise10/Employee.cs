9d55231cd7ab6cfdb9f2f1353e8a47fecd0c5bdc
michisalvo <jimmyginooo@libero.it>
2016-11-28 21:31:52 +0100

    Add Employee class

---
+++ Block4/Exercise10/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    public  abstract class Employee
+    {
+        public string empName;
+
+        public string Name
+        {
+            get {return empName; }
+            set {empName = value; }
+        }
+
+        public Employee(string name)
+        {
+            Name = empName;
+        }
+
+        public abstract int calcPaidCheck();                 
+    }
+}
