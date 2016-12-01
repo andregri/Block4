9e4c5167ab7afc9ee021dca99d75fab5b20292c4
michisalvo <jimmyginooo@libero.it>
2016-12-01 14:58:36 +0100

    Add class Book; create class Library

---
+++ Block4/Exercise02/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private string publisher;
+        private int numberISBN;
+        private DateTime releaseDate;
+        
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+        public int NumberISBN
+        {
+            get { return numberISBN; }
+            set { numberISBN = value; }
+        }
+        public DateTime ReleaseDate
+        {
+            get { return releaseDate; }
+            set { releaseDate = value; }
+        }
+
+        public Book(string title, string author, string publisher, int isbn, DateTime release)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            NumberISBN = isbn;
+            ReleaseDate = release;
+
+        }
+    }
+}
