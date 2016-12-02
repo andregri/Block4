c5136b77b5425ee7993df51116553ada2808e620
andregri <andrea.grillo96@live.com>
2016-12-03 00:23:12 +0100

    create closing tag handler

---
+++ Block4/Exercise03/CloseTagHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class CloseTagHandler : IHandler
+    {
+        public int Process(IContext context, string text)
+        {
+            TagContext tagContext = context as TagContext;
+            if (tagContext == null)
+            {
+                throw new ArgumentException("TagHandler needs TagContext.");
+            }
+
+            string tagName = text.Substring(2, text.Length - 2);
+            tagContext.PopTag();
+
+            return text.Length;
+        }
+    }
+}
