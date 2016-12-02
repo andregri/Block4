8f9801216277b2918bd68168344468009a999171
andregri <andrea.grillo96@live.com>
2016-12-03 00:04:14 +0100

    make TagContext class instead of private struct

---
+++ Block4/Exercise03/TagContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    public struct Tag
+    {
+        public string Name
+        {
+            get; set;
+        }
+        public string Attribute
+        {
+            get; set;
+        }
+        public int Value
+        {
+            get; set;
+        }
+
+        public Tag(string name, string attribute, int value)
+        {
+            this.Name = name;
+            this.Attribute = attribute;
+            this.Value = value;
+        }
+
+        public Tag(string name) : this(name, null, 0) { }
+    }
+
+    public class TagContext : IContext
+    {
+        private static Stack<Tag> tagStack;
+
+        public int Depth
+        {
+            get
+            {
+                return tagStack.Count;
+            }
+        }
+
+        public void PushTag(Tag tag)
+        {
+            if (tag != null)
+            {
+                tagStack.Push(tag);
+            }
+        }
+
+        public Tag PopTag()
+        {
+            return tagStack.Pop();
+        }
+    }
+}
