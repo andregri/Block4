385e251475d6ba2e50c0d246e30c72a5fb374487
andregri <andrea.grillo96@live.com>
2016-12-03 00:06:39 +0100

    create opening tag handler

---
+++ Block4/Exercise03/OpenTagHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    public class OpenTagHandler : IHandler
+    {
+        private string text;
+
+        public OpenTagHandler(string text)
+        {
+            this.text = text;
+        }
+
+        public string Process(IContext context, string text)
+        {
+            TagContext tagContext = context as TagContext;
+            if (tagContext == null)
+            {
+                return null;
+            }
+
+            int tagDelimiter = text.IndexOf('>');
+            string[] tagElements = text.Substring(1, tagDelimiter).Split(new char[] { '=', ' ' });
+
+            Tag tag;
+            if (tagElements.Length == 1)
+            {
+                tag = new Tag(tagElements[0]);
+            }
+            else
+            {
+                int len = tagElements[2].Length;
+                int attrValue = int.Parse(tagElements[2].Substring(0, len - 2));
+                tag = new Tag(tagElements[0], tagElements[1], attrValue);
+            }
+
+            tagContext.PushTag(tag);
+
+            return text.Substring(tagDelimiter + 1);
+        }
+    }
+}
