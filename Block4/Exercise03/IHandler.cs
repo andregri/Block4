3bf6541424e76de5eb31d2f2a25daf84bc0e97da
andregri <andrea.grillo96@live.com>
2016-12-02 22:48:31 +0100

    add handler and context interface

---
+++ Block4/Exercise03/IHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    interface IHandler
+    {
+        string Process(IContext context, string text);
+    }
+
+    interface IContext
+    {
+    }
+}
