376dd192e0033ab75f7c4fd22281a5e97aa3878b
andregri <andrea.grillo96@live.com>
2016-12-02 23:07:32 +0100

    create exception class for parsing errors

---
+++ Block4/Exercise03/XmlParseFileException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class XmlParseFileException : ApplicationException
+    {
+        private int line;
+        private string tag;
+        public override string Message
+        {
+            get
+            {
+                return String.Format("Expected closing tag {0} at line {1}.", tag, line);
+            }
+        }
+
+        public XmlParseFileException(int line, string tag)
+        {
+            this.line = line;
+            this.tag = tag;
+        }
+    }
+}
