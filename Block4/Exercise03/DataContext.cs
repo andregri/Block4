c027ef1713187953958fb6a3149a99c9eddd0560
andregri <andrea.grillo96@live.com>
2016-12-03 12:54:21 +0100

    add default handler with relative context

---
+++ Block4/Exercise03/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Exercise03.Handler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class DataContext : IContext
+    {
+        private List<string> data = new List<string>();
+
+        public void Push(object input)
+        {
+            data.Add((string)input);
+        }
+
+        public object Pop()
+        {
+            return data[data.Count - 1] ?? null;
+        }
+    }
+}
---
+++ Block4/Exercise03/Handler/DefaultHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03.Handler
+{
+    class DefaultHandler : IHandler
+    {
+        public int Process(IContext context, string text)
+        {
+            int closingTagDelimeter = text.IndexOf('<');
+            string value = text.Substring(0, text.Length - closingTagDelimeter);
+
+            context.Push(value);
+
+            return closingTagDelimeter;
+        }
+    }
+}
