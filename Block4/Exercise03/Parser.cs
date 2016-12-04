fb94623e42b07c93130d609d3314273c6117d689
andregri <andrea.grillo96@live.com>
2016-12-04 18:03:14 +0100

    add parser implementation

--- Block4/Exercise03/Parser.cs
+++ Block4/Exercise03/Parser.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using Exercise03.Handler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Exercise03
 {
     public class Parser
     {
+        private static Dictionary<char, IHandler> handlers;
+        private static IHandler defaultHandler;
+
+        private Dictionary<IHandler, IContext> contexts;
+        private IContext defaultContext;
+
+        public TagContext Tags
+        {
+            get;
+            private set;
+        }
+
+        public DataContext Data
+        {
+            get;
+            private set;
+        }
+
+        static Parser()
+        {
+            defaultHandler = new DefaultHandler();
+            handlers = new Dictionary<char, IHandler>();
+            handlers['<'] = new TagHandler();
+        }
+
+        public Parser()
+        {
+            Data = new DataContext();
+            Tags = new TagContext();
+            contexts = new Dictionary<IHandler, IContext>();
+            contexts[handlers['<']] = Tags;
+            defaultContext = Data;
+        }
+
+        public void Parse(StringReader reader)
+        {
+            int lineNumber = 0;
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                try
+                {
+                    ParseLine(line);
+                }
+                catch(MissingTagException e)
+                {
+                    throw new XmlParseFileException(lineNumber, e);
+                }
+                
+                lineNumber++;
+            }
+        }
+
+        public void ParseLine(string line)
+        {
+            int processedLine = 0;
+            IHandler h;
+            while (processedLine < line.Length)
+            {
+                if (!handlers.TryGetValue(line[0], out h))
+                {
+                    processedLine += h.Process(contexts[h], line);
+                }
+                else
+                {
+                    processedLine += defaultHandler.Process(defaultContext, line);
+                }
+            }
+        }
     }
 }
