b2d7e4031440039ebacb1be9853053520c9ae994
andregri <andrea.grillo96@live.com>
2016-12-04 22:17:29 +0100

    delete blank line

--- Block4/ClassTest/UnitTest09.cs
+++ Block4/ClassTest/UnitTest09.cs
@@ -26,7 +26,7 @@ namespace ClassTest
 
         [TestMethod]
         public void TestBeverageAndSnack()
-        {            
+        {
             Assert.AreEqual("Lizard Wine - Small: € 5,20, Medium: € 7,45, Large: € 9,60", menu[0].printToString());
             Assert.AreEqual("Bilk - Small: € 2,00, Medium: € 3,50, Large: € 5,00", menu[1].printToString());
             Assert.AreEqual("Coco Voda - Small: € 7,50, Medium: € 9,00, Large: € 12,00", menu[2].printToString());
@@ -34,7 +34,6 @@ namespace ClassTest
             Assert.AreEqual("Plasmon - Price: € 2,66", menu[4].printToString());
             Assert.AreEqual("Ringo - Price: € 1,04", menu[5].printToString());
             Assert.AreEqual("Mikado - Price: € 1,87", menu[6].printToString());
-
         }
     }
 }
