8f156a4d2ddf52c5f4bff0402dbbe568afd5d6eb
andregri <andrea.grillo96@live.com>
2016-12-04 12:44:41 +0100

    test open tag handler

--- Block4/ClassTest/UnitTest03.cs
+++ Block4/ClassTest/UnitTest03.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise03;
+using Exercise03.Handler;
 
 namespace ClassTest
 {
     [TestClass]
     public class UnitTest03
     {
+        string lineNoAttr = "<name>Peter</name>";
+        string lineWithAttr = "<name attr=\"1\">Peter</name>";
+
         [TestMethod]
-        public void TestMethod1()
+        public void OpeningTagTestNoAttribute()
         {
+            TagContext context = new TagContext();
+            OpenTagHandler handler = new OpenTagHandler();
+            int result = handler.Process(context, lineNoAttr);
+
+            Assert.AreEqual("name", ((Tag)context.Pop()).Name);
+            Assert.AreEqual(lineNoAttr.IndexOf('>') + 1, result);    
+        }
+
+        [TestMethod]
+        public void OpeningTagTestWithAttribute()
+        {
+            TagContext context = new TagContext();
+            OpenTagHandler handler = new OpenTagHandler();
+            int result = handler.Process(context, lineWithAttr);
+
+            Tag tag = (Tag)context.Pop();
+            Assert.AreEqual("name", tag.Name);
+            Assert.AreEqual("attr", tag.Attribute);
+            Assert.AreEqual(1, tag.Value);
+            Assert.AreEqual(lineWithAttr.IndexOf('>') + 1, result);
         }
     }
 }
